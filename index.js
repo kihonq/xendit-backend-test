9ee0b875ca57aab7b6ed11f89465f900af4d5f41
Ki Hong <firdausz@seekasia.com>
2022-05-18 22:23:35 +0800

    🔊 Add `winston` logger

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
+const logger = require('./src/services/logger');
+
 
 db.serialize(() => {
     buildSchemas(db);
 
     const app = require('./src/app')(db);
 
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+    app.listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
