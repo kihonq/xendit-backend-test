9cd6ba079f1e3734566f7b11d71e119456144bbb
Ki Hong <firdausz@seekasia.com>
2022-05-20 14:12:15 +0800

    ✅ Increase test coverage

--- src/routes/health.test.ts
+++ src/routes/health.test.ts
@@ -2,13 +2,11 @@ import request from 'supertest';
 
 import app from '../app';
 
-describe('API tests', () => {
-  describe('GET /health', () => {
-    it('should return 200', (done) => {
-      request(app)
-        .get('/health')
-        .expect('Content-Type', /text/)
-        .expect(200, done);
-    });
+describe('GET /health', () => {
+  it('should return 200', (done) => {
+    request(app)
+      .get('/health')
+      .expect('Content-Type', /text/)
+      .expect(200, done);
   });
 });
